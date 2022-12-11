6764b44936ae57a9288b2b24de7af74586c2a64f
hanatani <adgjmptwhanatani@gmail.com>
2022-12-12 08:21:41 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+
+      ),
+    );
+  }
+}
+
+//stless statelesswidjet を追加することで処理が爆速になる
